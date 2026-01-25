6c4b28a1ebfa0c9d8e6c98616a4d4b91607fada1
Hacker-Unity <hackerunity.community@gmail.com>
2026-01-25 23:21:35 +0530

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -255,7 +255,7 @@ const Footer = () => {
                 <motion.div
                   variants={iconVariants}
                   whileHover="hover"
-                  className="p-2.5 bg-primary-dark/70 rounded-full mr-4 text-accent group-hover:text-white group-hover:bg-accent transition-colors"
+                  className="p-2.5 bg-gray-200 dark:bg-primary-dark/70 rounded-full mr-4 text-accent group-hover:text-white group-hover:bg-accent transition-colors"
                 >
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
