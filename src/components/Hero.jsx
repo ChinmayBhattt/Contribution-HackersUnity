dda8705d7fcad435765e81dda436b5e580d305c5
Hacker-Unity <hackerunity.community@gmail.com>
2026-04-16 21:46:58 +0530

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,6 @@
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import { useAuth } from '@clerk/clerk-react';
-
 const Hero = () => {
-  const { isSignedIn } = useAuth();
 
   // Animation variants
   const fadeInUp = {
@@ -150,23 +147,6 @@ const Hero = () => {
               initial="hidden"
               animate="visible"
             >
-              {isSignedIn ? (
-                <motion.div variants={fadeInUp}>
-                  <Link to="/dashboard">
-                    <motion.button
-                      // className="btn btn-primary inline-flex items-center space-x-2"
-                      variants={buttonHover}
-                      whileHover="hover"
-                      whileTap="tap"
-                    >
-                      {/* <span>Go to Dashboard</span> */}
-                      {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-                      </svg> */}
-                    </motion.button>
-                  </Link>
-                </motion.div>
-              ) : (
                 <motion.div variants={fadeInUp}>
                   <Link to="/sign-up">
                     <motion.button
@@ -182,7 +162,6 @@ const Hero = () => {
                     </motion.button>
                   </Link>
                 </motion.div>
-              )}
 
               <motion.div variants={fadeInUp}>
                 <Link to="/events">
